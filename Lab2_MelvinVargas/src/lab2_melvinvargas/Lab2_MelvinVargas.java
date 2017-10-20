e3c63fa08905fd37913414ceea4bee566ed3a890
Melvin <melvinrenevargas19@gmail.com>
2017-10-20 14:19:31 -0600

    Creacion de menu agregar

--- Lab2_MelvinVargas/src/lab2_melvinvargas/Lab2_MelvinVargas.java
+++ Lab2_MelvinVargas/src/lab2_melvinvargas/Lab2_MelvinVargas.java
@@ -1,39 +1,63 @@
-
 package lab2_melvinvargas;
 
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
-
 public class Lab2_MelvinVargas {
 
-  
     public static void main(String[] args) {
-        String opcion="";
-        ArrayList <Maestros>maestros=new ArrayList();
-        ArrayList<Clases> clases=new ArrayList();
-        while(!opcion.equals("e")){
-            opcion=JOptionPane.showInputDialog(""
-            +"a-Administracion\n"
-            +"b-Matricula\n"
-            +"c-Log in\n"
-            +"d-Agregar\n"
-            +"e-Salir"
-                        );
-        }   
-        if(opcion.equals("a")){
-            
+        String opcion = "";
+        ArrayList maestros = new ArrayList();
+        ArrayList clases = new ArrayList();
+        Maestros m=new Maestros();
+        Clases c=new Clases();
+        while (!opcion.equals("e")) {
+            opcion = JOptionPane.showInputDialog(""
+                    + "a-Administracion\n"
+                    + "b-Matricula\n"
+                    + "c-Log in\n"
+                    + "d-Agregar\n"
+                    + "e-Salir"
+            );
+            if (opcion.equals("a")) {
+
+            }
+            if (opcion.equals("b")) {
+
+            }
+            if (opcion.equals("c")) {
+
+            }
+            if (opcion.equals("d")) {
+                while (!opcion.equals("c")) {
+                    opcion = JOptionPane.showInputDialog(""
+                            + "a-Agregar maestro\n"
+                            + "b-Agregar clases\n"
+                            + "c-Volver al menu principal\n"
+                    );
+                    if (opcion.equals("a")) {
+                        String nombre=JOptionPane.showInputDialog("Ingrese nombre del maestro");
+                        String titulo=JOptionPane.showInputDialog("Ingrese titulo del maestro");
+                        String maestria=JOptionPane.showInputDialog("Ingrese maestria del maestro");
+                        String salario=JOptionPane.showInputDialog("Ingrese salario del maestro");
+                        String usuario=JOptionPane.showInputDialog("Ingrese usuario del maestro");
+                        String contrasena=JOptionPane.showInputDialog("Ingrese contrasena del usuario");
+                        String cantidad=JOptionPane.showInputDialog("Ingrese cantidad de clases del maestro");
+                        maestros.add(new Maestros(nombre,titulo,maestria,salario,usuario,contrasena,cantidad));
+                        System.out.print(maestros);
+                    }
+                    if (opcion.equals("b")) {
+                        String nombre=JOptionPane.showInputDialog("Ingrese nombre de la clase");
+                        String seccion=JOptionPane.showInputDialog("Ingrese la seccion de la clase");
+                        int alumnos=Integer.parseInt("Ingrese cantidad  maxima de alumnos de la clase");
+                        String maestro=JOptionPane.showInputDialog("Ingrese el maestro de la clase");
+                        int uv=Integer.parseInt("Ingrese unidades valorativas de la clase");
+                        clases.add(new Clases(nombre,seccion,alumnos,maestro,uv));
+                    }    
+                }
+            }
         }
-        if(opcion.equals("b")){
-            
-        }
-        if(opcion.equals("c")){
-            
-        }
-        if(opcion.equals("d")){
-            
-        }
-            
+
     }
-    
+
 }
--- Lab2_MelvinVargas/src/lab2_melvinvargas/Maestros.java
+++ Lab2_MelvinVargas/src/lab2_melvinvargas/Maestros.java
@@ -9,17 +9,19 @@ public class Maestros {
     String salario;
     String usuario;
     String contrasena;
+    String cantidad;
 
     public Maestros() {
     }
 
-    public Maestros(String nombre, String titulo, String maestria, String salario, String usuario, String contrasena) {
+    public Maestros(String nombre, String titulo, String maestria, String salario, String usuario, String contrasena,String cantidad) {
         this.nombre = nombre;
         this.titulo = titulo;
         this.maestria = maestria;
         this.salario = salario;
         this.usuario = usuario;
         this.contrasena = contrasena;
+        this.cantidad=cantidad;
     }
 
     public String getNombre() {
@@ -68,6 +70,20 @@ public class Maestros {
 
     public void setContrasena(String contrasena) {
         this.contrasena = contrasena;
+    }
+
+    public String getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(String cantidad) {
+        this.cantidad = cantidad;
+    }
+    
+
+    @Override
+    public String toString() {
+        return "Maestros: " + "nombre=" + nombre + ", usuario=" + usuario + ", contrasena=" + contrasena + "||";
     }
     
 }
